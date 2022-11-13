e8fcb293304167aeaf0512540237d6b9e0c8efd4
JohnMark van Niekerk <jmvan.niekerk@gmail.com>
2022-11-14 00:13:58 +0200

    added game initialisation scripts

---
+++ scripts/create_logger.js
@@ -0,0 +1,36 @@
+/**
+ * @typedef { import('./bitburner.d').NS } NS
+ * @typedef {'info' | 'warning' | 'error' | 'success'} LogLevel
+ */
+
+/** @param {NS} ns */
+export default function createLogger(ns) {
+    /** @type {{level: LogLevel | null, msg: string | null}} */
+    let lastLog = { level: null, msg: null };
+    /**
+     * @param {string} msg
+     * @param {LogLevel} level
+     */
+    return function log(msg, level = 'info') {
+      if (level === lastLog.level && msg === lastLog.msg) {
+        return;
+      }
+      switch (level) {
+        case 'info':
+          ns.print(`[INFO] ${msg}`);
+          break;
+        case 'warning':
+          ns.print(`[WARN] ${msg}`);
+          break;
+        case 'error':
+          ns.print(`[ERR ] ${msg}`);
+          break;
+        case 'success':
+          ns.print(`[ OK ] ${msg}`);
+          break;
+        default:
+          throw new Error('Unhandled log level');
+      }
+      lastLog = { level, msg };
+    };
+  }
---
+++ scripts/initialise_game_directory.js
@@ -0,0 +1,78 @@
+/**
+ * @typedef { import('./bitburner.d').NS } NS
+ * @typedef {{path: string, mode: string, type: 'tree' | 'blob', sha: string, url: string}} File
+ * @typedef {{sha: string, url: string, tree: File[], truncated: boolean}} Repo
+ * @typedef {{branch: string, watch: boolean}} ScriptOptions
+ * */
+
+ import createLogger from './create_logger.js';
+
+ const INTERVAL = 1_000;
+ 
+ /** @param {NS} ns **/
+ export async function main(ns) {
+   ns.disableLog('disableLog');
+   ns.disableLog('wget');
+   ns.disableLog('sleep');
+   ns.disableLog('exit');
+ 
+   const log = createLogger(ns);
+   /** @type {ScriptOptions} */
+   const { branch, watch } = ns.flags([
+     ['branch', 'main'], // which branch to pull from
+     ['watch', false], // enable watch mode
+   ]);
+ 
+   /**
+    * @param {string} branch
+    * @param {string} manifestName
+    * @returns {Promise<Repo | null>}
+    */
+   const fetchRepo = async (branch, manifestName) => {
+     const repoUrl = `https://api.github.com/repos/Kusp00110110/bitburner-workforce/trees/${branch}?recursive=1`;
+     if (await ns.wget(repoUrl, manifestName)) {
+       return JSON.parse(ns.read(manifestName));
+     }
+     return null;
+   };
+   /** @param {Repo} repo */
+   const fetchFiles = async (repo) => {
+     for (const file of repo.tree) {
+       if (file.type !== 'blob' || file.path.endsWith('.js') === false) {
+         continue;
+       }
+       const fileUrl = `https://raw.githubusercontent.com/poteto/bitburner-scripts/${repo.sha}/${file.path}`;
+       if (await ns.wget(fileUrl, file.path)) {
+         log(`Synced ${file.path} (${file.sha.slice(0, 7)})`, 'success');
+         continue;
+       }
+       log(`Couldn't get ${file.path}`, 'error');
+     }
+   };
+ 
+   let lastRepoSha = null;
+   while (true) {
+     const repo = await fetchRepo(branch, 'repo.json.txt');
+     if (repo == null) {
+       log(`Couldn't get repo manifest`, 'error');
+       return ns.exit();
+     }
+     if (lastRepoSha !== repo.sha) {
+       await fetchFiles(repo);
+       lastRepoSha = repo.sha;
+       log(
+         `Sync for branch ${branch} (${repo.sha.slice(
+           0,
+           7
+         )}) completed at: ${new Date().toLocaleString()}`,
+         'success'
+       );
+       ns.tprint(`Succesfully fetched ${repo.sha.slice(0, 7)}`);
+     }
+     if (watch === false) {
+       return ns.exit();
+     }
+     log('Watching for changes...');
+     await ns.sleep(INTERVAL);
+   }
+ }
